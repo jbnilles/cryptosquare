b482bcd65e4521cc1c0e2cc19c92c2d360ae6d5e
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-20 16:25:47 -0700

    it will make the cryptofunction work and print the answer
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 function crypto (x){
  let newStr =x.replace(/\s|\W/g,"");
-  //x.replaceAll(/\w/,"");
-  //console.log(x);
-  let box = [];
-  let columns = (Math.ceil(Math.sqrt(newStr.length)));
-  for(let i = 0; i < newStr.length ; ){
+ let test = [[1,2,3,4],[5,6,7,8],[9,0]]                     //test    test[1][2]
+  let box = [];                                             //0  [1,2,3,4]
+  let columns = (parseInt(Math.sqrt(newStr.length)));    //let columns = (Math.ceil(Math.sqrt(newStr.length))) - 1;      //1   [5,6,7,8]
+  for(let i = 0; i < newStr.length ; ){                     //2   [9,0]
     let tempArray = [];
     for(let j = 0; j < columns; i++, j++){
       if(i >= newStr.length)
@@ -20,13 +19,25 @@ function crypto (x){
   }
   return box;
 }
-function printCrypto(box){
-
-  for(let i = 0; i < box.length; i++)
+function printCrypto(box){ //not working yet
+  let resultStr = '';
+  for(let i = 0; i < box[0].length; i++)
   {
-    for(let j = 0; j < box[i].length; j++ ) {
-      console.log(box[j][i]);
+    for(let j = 0; j < box.length; j++ ) {
+    	if(box[j][i] !== undefined) {
+        if(resultStr.length % 6 === 0)
+        {
+          resultStr += ' ';
+          resultStr += box[j][i];
+        }
+        else
+        {
+          resultStr += box[j][i];
+        }
+      }
     }
   }
+  return resultStr;
 }
+//crypto("don't compare yourself to others, compare yourself to the person you were yesterday");
 //printCrypto(crypto("don't compare yourself to others, compare yourself to the person you were yesterday"));
